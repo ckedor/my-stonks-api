96b2b1d91e8f1f040302364d35b4b422b3e4c7b0
Christian Kedor <christian.kedor@gmail.com>
2026-04-04 16:09:13 -0300

    fix: make build.sh executable

--- backend/build.sh
+++ backend/build.sh
@@ -2,9 +2,11 @@
 set -e
 
 echo "==> Installing Poetry"
+
+export POETRY_HOME="/tmp/poetry"
 curl -sSL https://install.python-poetry.org | python3
 
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="$POETRY_HOME/bin:$PATH"
 
 echo "==> Installing dependencies"
 poetry install --no-interaction --no-root
